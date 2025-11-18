cf2665b8ebbf728ee0d1854ebfa3760f86f60a8a
Michael Lahr <michael.lahr@gmail.com>
2025-11-18 20:59:11 +0800

    feat: add virtual environment setup to run script

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 set -e
+
+python -mvenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
 for f in $(find examples/ -name '*.py'); do
     echo "Running $f..."
     python "$f"
